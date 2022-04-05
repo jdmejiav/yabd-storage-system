5b87af2ad25d02c35ecdbbd6da736b529c911993
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2022-04-04 23:36:29 -0500

    Update client.py

--- client/client.py
+++ client/client.py
@@ -83,53 +83,89 @@ class Client:
         return datarev
 
     def read(self,datakey=''):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.HOST,self.PORT))
-        #client_socket.setblocking(False)
-        key_obj=''
-        if datakey =='':
-            key_obj = input("Enter key of the object: ")
-        else:
-            key_obj = datakey
-        message = ''
-
-        getMessage ={   
-            "key": key_obj,
-            "method": "GET",
-            "op":self.GETOPERATION 
-        }
-        message = json.dumps(getMessage)
-
-        if message:
-            message = message.encode("utf-8")
-            message_header = f"{len(message):<{self.HEADER_LENGTH}}".encode("utf-8")
-            client_socket.sendall(message_header + message)
-
-        datarev=b''
-        message_header = client_socket.recv(30)
-        message_length = int(message_header.decode("utf-8").strip())
-        with alive_bar(message_length,title=f'Downloading') as bar:   # default setting
-            while len(datarev)<message_length:
-                res_data=client_socket.recv(30720)
-                datarev+=res_data
-                bar(len(res_data)) 
-            res=json.loads(datarev)
-        if "status" in res:
-            if res["status"] ==-1:
-                print('\033[91m' + res["message"] + '\033[0m')
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((self.HOSTSENSEI,self.PORTSENSEI))
+            #client_socket.setblocking(False)
+            key_obj=''
+            if datakey =='':
+                key_obj = input("Enter key of the object: ")
             else:
-                if(res["ext"]!='.yadbdatatxt'):
-                    with open(f'{res["name"]}{res["ext"]}', "wb") as new_file:
-                            new_file.write(base64.b64decode(res['data']))
-                    client_socket.close()
-                    print('\033[92m' + "Success!! data saved on file "+res["name"]+res["ext"]+ '\033[0m')
-                else:
-                    print('\033[92m' + "Success!! data recovered" +'\033[0m')
-                    print("Data->",base64.b64decode(res['data']).decode())
-                    client_socket.close()
-        del client_socket
-        return datarev
-    
+                key_obj = datakey
+            message = ''
+
+            getMessage ={   
+                "key": key_obj,
+                "method": "GET",
+            }
+            message = json.dumps(getMessage)
+            resources_file = message.encode("utf-8")  
+            message_header = f"{len(resources_file):<{30}}".encode("utf-8") 
+            client_socket.send(message_header+resources_file)
+            
+            message_header1 = client_socket.recv(30)        
+            message_length = int(message_header1.decode("utf-8").strip())
+            datares=b''
+            while len(datares)<message_length:
+                datares += client_socket.recv(1024)
+            client_socket.close()
+            
+            dict= json.loads(datares)
+
+            resources_file=dict['data'][0][key_obj]['resources']
+            nodes= dict['data'][1]
+            data = ''.encode()
+            for key, value in reversed(resources_file.items()):
+                ip=nodes['f']
+                node_to_get=()
+                i=0
+                while i <len(value):
+                    if ip[value[i]][1]==1:
+                        node_to_get=(value[i],ip[value[i]][0])
+                        i=len(value)+1
+                    i+=1
+
+                print(f'Recovering data from {node_to_get}')
+                getMessage ={   
+                    "key": key,
+                    "method": "GET",
+                }
+                message = json.dumps(getMessage)
+                message = message.encode("utf-8")
+                message_header = f"{len(message):<{30}}".encode("utf-8")
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.connect(node_to_get)
+                    sock.send(message_header + message)
+
+                    print(f'connected with {value[0]}, {ip[value[0]][0]}')
+
+                    message_header=b''
+
+                    message_header = sock.recv(30)
+                    message_length = int(message_header.decode("utf-8").strip())
+                    data_node=''.encode()
+                    with alive_bar(message_length,title=f'Downloading {key}') as bar:   # default setting
+                        while len(data_node)<message_length:
+                            res_data=sock.recv(30720)
+                            data_node += res_data
+                            bar(len(res_data)) 
+                    sock.send(b'ok')                       
+                    sock.close()
+                    data+=data_node
+                    del sock
+            fname= dict['data'][0][key_obj]['name']
+            fext= dict['data'][0][key_obj]['format']
+            if(fext!='.yadbdatatxt'):
+                with open(f'{fname}{fext}', "wb") as new_file:
+                        new_file.write(base64.b64decode(data))
+                print('\033[92m' + "Success!! data saved on file "+fname+fext+ '\033[0m')
+            else:
+                print('\033[92m' + "Success!! data recovered" +'\033[0m')
+                print("Data->",base64.b64decode(data).decode())
+        except:
+            print('\033[93m' + "Warning!! key not exists" +'\033[0m')
+
+
     def update(self,datakey='',datavalue=''):
         message = ''
         if datakey == '':
@@ -310,15 +346,17 @@ class Client:
                 sleep(2)
                 client.interface()
 
-    def __init__(self,HOST,PORT):
+    def __init__(self,HOST,PORT,HOSTSENSEI,PORTSENSEI):
         self.HOST = HOST
         self.PORT = PORT
+        self.HOSTSENSEI=HOSTSENSEI
+        self.PORTSENSEI=PORTSENSEI
         self.HEADER_LENGTH = 30
         self.GETOPERATION = 0
         self.objects = {}
 
 if __name__=='__main__':
-    client = Client("35.171.27.171",8001)
+    client = Client("127.0.0.1",8001,'127.0.0.1',8000)
     logo()
     client.interface()
     #client.create("camilo","juega lol")
